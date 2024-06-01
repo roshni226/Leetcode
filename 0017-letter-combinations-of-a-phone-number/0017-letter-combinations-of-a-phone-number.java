4d6dc330c36c089403612beaff20b7e50cb242e6
Roshni Joshi <roshu.joshi@gmail.com>
2024-06-01 18:44:49 +0530

    Time: 5 ms (37.50%), Space: 42 MB (48.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,30 +1,32 @@
 import java.util.*;
 class Solution {
-    String keypad[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-   public List<String> printComb(String s, int i, List<String> ans, StringBuilder comb) {
-        if (i == s.length()) {
-            ans.add(comb.toString());
-            return ans;
+    static String keypad[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public static void printComb(String s, int i, List<String> temp, String comb)
+    {
+        if(i==s.length())
+        {
+            temp.add(comb);
+            return;
         }
+        
+        char curr= s.charAt(i);
+        String keyval= keypad[curr-'0'];
 
-        char curr = s.charAt(i);
-        String keyval = keypad[curr - '0'];
-
-        for (int a = 0; a < keyval.length(); a++) {
-            comb.append(keyval.charAt(a));
-            printComb(s, i + 1, ans, comb);
-            comb.deleteCharAt(comb.length() - 1); // backtrack
+        for(int a=0;a<keyval.length();a++)
+        {
+            printComb(s,i+1,temp,comb+keyval.charAt(a));
         }
-
-        return ans; // Ensure we return the accumulated answers
-}
+        return;
+    }
     public List<String> letterCombinations(String digits){
         List<String> temp = new ArrayList<>();
-        if (digits.isEmpty()) {
-            return temp; // Return empty list if input is empty
+        if(digits.length()==0)
+        {
+            return temp;
         }
-        StringBuilder comb=new StringBuilder();;
-        return printComb(digits,0,temp,comb);
+        
+        printComb(digits,0,temp,"");
+        return temp;
         
     }
 }
