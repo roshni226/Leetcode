554c556e783ec353f48b8dd7be6627248abab80d
Roshni Joshi <roshu.joshi@gmail.com>
2024-06-05 18:20:51 +0530

    Time: 7 ms (72.51%), Space: 42.9 MB (50.21%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n=nums.length;
+        int total= Arrays.stream(nums).sum();
+        if((target+total)%2!=0 || total<target || total+target<0)
+        {
+            return 0;
+        }
+        int s2= (total+target)/2;
+        int zeroCount=0,k=0;
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0)
+            {
+                zeroCount++;
+            }
+            else
+            {
+                arr[k++]=nums[i];
+            }
+        }
+        int dp[][]= new int[n+1][s2+1];
+        dp[0][0]=1;
+        for(int i=1;i<=k;i++)
+        {
+            for(int j=0;j<=s2;j++)
+            {
+                if(j>=arr[i-1])
+                {
+                    dp[i][j]=dp[i-1][j-arr[i-1]]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        //possible ways for 0's
+        int zero= (int)Math.pow(2,zeroCount);
+        return dp[k][s2]*zero;
+    }
+}
