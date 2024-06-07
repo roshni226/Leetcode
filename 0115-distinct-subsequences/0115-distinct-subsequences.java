d1d74a51a8c15afe15e5da52071d7207a13d9186
Roshni Joshi <roshu.joshi@gmail.com>
2024-06-07 13:47:29 +0530

    Time: 15 ms (49.15%), Space: 48.6 MB (73.06%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,19 +1,19 @@
 class Solution {
     static int countUtil(String s1, String s2, int ind1, int ind2, int[][] dp) {
-        // If we have exhausted s2, there's one valid subsequence (empty string) in s1.
-        if (ind2 < 0)
+        // If we have exhausted s2, there's one valid subsequence (empty string) in s1.  
+        if (ind2==0)
             return 1;
-        // If we have exhausted s1 but not s2, there are no valid subsequences.
-        if (ind1 < 0)
+        if (ind1 ==0 )
             return 0;
-
-        // If the result is already computed, return it.
         if (dp[ind1][ind2] != -1)
             return dp[ind1][ind2];
+        // If we have exhausted s1 but not s2, there are no valid subsequences.
+        
+        // If the result is already computed, return it.
 
         // If the characters at the current positions match, we can either leave one character from s1
         // or continue to the next character in s1 while staying at the same character in s2.
-        if (s1.charAt(ind1) == s2.charAt(ind2)) {
+        if (s1.charAt(ind1-1) == s2.charAt(ind2-1)) {
             int leaveOne = countUtil(s1, s2, ind1 - 1, ind2 - 1, dp);
             int stay = countUtil(s1, s2, ind1 - 1, ind2, dp);
 
@@ -26,12 +26,12 @@ class Solution {
     }
     static int subsequenceCounting(String s1, String s2, int lt, int ls) {
         // Initialize a DP array to store intermediate results
-        int dp[][] = new int[lt][ls];
+        int dp[][] = new int[lt+1][ls+1];
         for (int rows[] : dp)
             Arrays.fill(rows, -1);
 
         // Call the recursive helper function to compute the count
-        return countUtil(s1, s2, lt - 1, ls - 1, dp);
+        return countUtil(s1, s2, lt, ls , dp);
     }
     public int numDistinct(String s, String t) {
          return subsequenceCounting(s, t, s.length(), t.length());
