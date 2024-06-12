93b9079bf0c81b0179f6d5c68ef6a8c831f06de2
Roshni Joshi <roshu.joshi@gmail.com>
2024-06-12 12:25:01 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (49.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,32 @@
 class Solution {
-    public int func(int nums[],int n, int dp[])
-    {
-        if(n==nums.length||n==nums.length+1)
-        {
-            return 0;
-        }
-        if(dp[n]!=-1) return dp[n];
-        //take or not take
-        int take=nums[n]+func(nums,n+2,dp);
-        int notTake=func(nums,n+1,dp);
-        int ans= Integer.max(take,notTake);
-        return dp[n]=ans;
-    }
+    // public int func(int nums[],int n, int dp[])
+    // {
+    //     if(n==nums.length||n==nums.length+1)
+    //     {
+    //         return 0;
+    //     }
+    //     if(dp[n]!=-1) return dp[n];
+    //     //take or not take
+    //     int take=nums[n]+func(nums,n+2,dp);
+    //     int notTake=func(nums,n+1,dp);
+    //     int ans= Integer.max(take,notTake);
+    //     return dp[n]=ans;
+    // }
+    // public int rob(int[] nums) {
+    //     int dp[]= new int[nums.length+1];
+    //     Arrays.fill(dp,-1);
+    //     return func(nums,0,dp);
+    // }
     public int rob(int[] nums) {
         int dp[]= new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return func(nums,0,dp);
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=nums.length;i++)
+        {
+            int take= nums[i-1]+dp[i-2];
+            int notTake= dp[i-1];
+            dp[i]=Integer.max(take,notTake);
+        }
+        return dp[nums.length];
     }
 }
