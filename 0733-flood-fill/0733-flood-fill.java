081f75a52260071eebec62ad84a0d37e4249a197
Roshni Joshi <roshu.joshi@gmail.com>
2024-07-21 23:07:36 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (86.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,41 +1,30 @@
-class Pair{
-    int r;
-    int c;
-    public Pair(int r, int c)
-    {
-        this.r=r;
-        this.c=c;
-    }
-}
 class Solution {
+    public void dfs(int [][] image, int stcolor, int color, int i, int j, int n, int m)
+    {
+        image[i][j]=color;
+        int dx[]= {0,0,1,-1};
+        int dy[]= {1,-1,0,0};
+        for(int k=0;k<4;k++)
+        {
+            int x= i+dx[k];
+            int y= j+dy[k];
+            if (x >= 0 && y >= 0 && x < n && y < m && image[x][y] == stcolor) {
+                dfs(image, stcolor, color, x, y, n, m);
+            }
+            
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n= image.length;
         int m= image[0].length;
-        int vis[][]= new int[n][m];
-        Queue<Pair> queue= new LinkedList<Pair>();
-        queue.add(new Pair(sr,sc));
-        vis[sr][sc]=1;
-        int stcolor=image[sr][sc];
-        int dx[]={0,0,1,-1};
-        int dy[]={1,-1,0,0};
-        while(!queue.isEmpty())
+        
+        
+        int stcolor= image[sr][sc];
+        if(stcolor!=color)
         {
-            Pair curr= queue.poll();
-            int r=curr.r;
-            int c= curr.c;
-            image[r][c]=color;
-            for(int j=0;j<4;j++)
-            {
-                int x= r+dx[j];
-                int y= c+dy[j];
-                if(x<0||y<0||x>=n||y>=m||image[x][y]!=stcolor||vis[x][y]==1)
-                {
-                    continue;
-                }
-                vis[x][y]=1;
-                queue.add(new Pair(x,y));
-            }
+         dfs(image,stcolor,color,sr,sc,n,m);   
         }
         return image;
+        
     }
 }
