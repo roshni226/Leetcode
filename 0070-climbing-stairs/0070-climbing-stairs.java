2ec66fd86a16564cbfe9f7d5e36c9c31c0a2915c
Roshni Joshi <roshu.joshi@gmail.com>
2024-07-23 18:04:06 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (29.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1) return 1;
-        int n1=1,n2=1,n3=0;
+        int dp[]= new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
         for(int i=2;i<=n;i++)
         {
-            n3=n1+n2;
-            n1=n2;
-            n2=n3;
+            //ways(n)= ways(n-1)+ways(n-2)
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return n3;
+        return dp[n];
     }
 }
