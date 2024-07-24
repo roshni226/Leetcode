c1f1370511350882da61a181ed7924d31743a118
Roshni Joshi <roshu.joshi@gmail.com>
2024-07-24 23:06:44 +0530

    Time: 63 ms (29.27%), Space: 74.6 MB (22.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,31 +1,36 @@
 class Solution {
-    public int lis(int[] nums, int n,int ind,int prev, int dp[][])
-    {
-        if(ind==n+1)
-        {
-            return 0;
-        }
-        if(dp[ind][prev]!=-1) return dp[ind][prev];
-        
-        if(prev==0||nums[ind-1]>nums[prev-1])
-        {
-            dp[ind][prev]=Integer.max(1+lis(nums,n,ind+1,ind,dp),
-                                          lis(nums,n,ind+1,prev,dp));
-        }
-        else
-        {
-            dp[ind][prev]=lis(nums,n,ind+1,prev,dp);
-        }
-        return dp[ind][prev];
-    }
-        
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int dp[][]= new int [n+1][n+1];
-        for(int i=0;i<=n;i++)
+        int n= nums.length;
+        HashSet<Integer> set= new HashSet<>();
+        for(int i=0;i<n;i++)
         {
-            Arrays.fill(dp[i],-1);
+            set.add(nums[i]);
         }
-        return lis(nums,n,1,0,dp);
+        int m= set.size();
+        int nums2[]= new int[m];
+        int a=0;
+        for(int curr:set)
+        {
+            nums2[a++]=curr;
+        }
+            
+        Arrays.sort(nums2);
+        
+        int dp[][]= new int[n+1][m+1];
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(nums[i-1]==nums2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
