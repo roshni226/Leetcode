1ddd7d715e6c4a857711ff50ed337f5cd95c07c5
Roshni Joshi <roshu.joshi@gmail.com>
2024-07-24 19:40:18 +0530

    Time: 32 ms (8.74%), Space: 50.8 MB (12.86%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,62 +1,70 @@
 class Solution {
-    public String shortestCommonSupersequence(String str1, String str2) {
-        int n= str1.length();
-        int m=str2.length();
+    public String shortestCommonSupersequence(String s1, String s2) {
+        int n= s1.length();
+        int m= s2.length();
         
         int dp[][]= new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
         
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
             {
-                if(str1.charAt(i-1)==str2.charAt(j-1))
+                if(s1.charAt(i-1)==s2.charAt(j-1))
                 {
-                    dp[i][j]=dp[i-1][j-1]+1;
+                    dp[i][j]=1+dp[i-1][j-1];
                 }
                 else
                 {
-                    dp[i][j]=Integer.max(dp[i-1][j],dp[i][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        int lcs=dp[n][m];
-        //length of scs
+        
+        int lcs= dp[n][m];
+        
         int scs=n+m-lcs;
+        
         int i=n,j=m;
         String s="";
         while(i>0 && j>0)
         {
-            if(str1.charAt(i-1)==str2.charAt(j-1))
+            if(s1.charAt(i-1)==s2.charAt(j-1))
             {
-                s=str1.charAt(i-1)+s;
+                s=s1.charAt(i-1)+s;
                 i--;
                 j--;
             }
+            else if(dp[i-1][j]>dp[i][j-1])
+            {
+                s=s1.charAt(i-1)+s;
+                i--;
+            }
             else
             {
-                if(dp[i-1][j]>dp[i][j-1])
-                {
-                    s=str1.charAt(i-1)+s;
-                    i--;
-                }
-                else
-                {
-                    s=str2.charAt(j-1)+s;
-                    j--;                }
+                s=s2.charAt(j-1)+s;
+                j--;
             }
         }
-        
         while(i>0)
         {
-            s=str1.charAt(i-1)+s;
+            s=s1.charAt(i-1)+s;
             i--;
         }
         while(j>0)
         {
-            s=str2.charAt(j-1)+s;
+            s=s2.charAt(j-1)+s;
             j--;
         }
         return s;
-        
     }
 }
