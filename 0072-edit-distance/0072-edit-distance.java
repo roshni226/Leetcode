ea1cb7efde5d9c2dc279a5a811ea0046eb942d5a
Roshni Joshi <roshu.joshi@gmail.com>
2024-07-24 20:24:32 +0530

    Time: 7 ms (8.26%), Space: 45.1 MB (16.62%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,34 +1,17 @@
 class Solution {
-    public int min(int a,int b,int c)
-    {
-        if(a>=b && c>=b)
-        {
-            return b;
-        }
-        else if(b>=a &&c>=a)
-        {
-            return a;
-        }
-        else 
-        {
-            return c;
-        }
-    }
     public int minDistance(String word1, String word2) {
         int n= word1.length();
-        int m=word2.length();
+        int m= word2.length();
+        
         int dp[][]= new int[n+1][m+1];
-        
-        //initialization
-        for(int i=0;i<=n;i++)//length of s2 is 0
+        for(int i=0;i<=n;i++)
         {
-            dp[i][0]=i;
+            dp[i][0]=i;//len of s2 is 0
         }
-        for(int j=0;j<=m;j++)//length of s1 is 0
+        for(int j=0;j<=m;j++)
         {
-            dp[0][j]=j;
+            dp[0][j]=j;//len of s1 is 0
         }
-        
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=m;j++)
@@ -39,10 +22,11 @@ class Solution {
                 }
                 else
                 {
-                    dp[i][j]=min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])+1;
+                    dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
                 }
             }
         }
+        
         return dp[n][m];
     }
 }
