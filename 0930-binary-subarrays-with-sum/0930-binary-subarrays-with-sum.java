df42ef11b3de5fe23792f11ab7820e9b542d6abe
Roshni Joshi <roshu.joshi@gmail.com>
2024-08-15 16:22:39 +0530

    Time: 1 ms (100.00%), Space: 48 MB (33.78%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,20 +1,26 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
-    int ans = 0;
-    int prefix = 0;
-    // {prefix: number of occurrence}
-    Map<Integer, Integer> count = new HashMap<>();
-    count.put(0, 1);
-
-    for (final int num : nums) {
-      prefix += num;
-      final int key = prefix - goal;
-      if (count.containsKey(key))
-        ans += count.get(key);
-      count.merge(prefix, 1, Integer::sum);
+    public int util(int[] nums, int k)
+    {
+        //function to find count of subarrays with sum<=goal
+        if(k<0) return 0;
+        int n= nums.length;
+        int l=0,r=0,sum=0,ans=0;
+        
+        while(r<n)
+        {
+            sum+=nums[r];
+            while(sum>k && l<=r)
+            {
+                sum-=nums[l];
+                l++;
+            }
+                      
+            ans=ans+(r-l+1);
+            r++;
+        }
+        return ans;
     }
-
-    return ans;
- 
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return util(nums,goal)-util(nums,goal-1);
     }
 }
