59d22654ab5f0c7ae4c73b721984a5f36c668325
Roshni Joshi <roshu.joshi@gmail.com>
2024-09-24 08:07:23 +0530

    Time: 2 ms (98.63%), Space: 45.2 MB (5.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,56 +1,22 @@
-// class Solution {
-//   public int[] twoSum(int[] nums, int target) {
-//     Map<Integer, Integer> numToIndex = new HashMap<>();
-
-//     for (int i = 0; i < nums.length; ++i) {
-//       if (numToIndex.containsKey(target - nums[i]))
-//         return new int[] {numToIndex.get(target - nums[i]), i};
-//       numToIndex.put(nums[i], i);
-//     }
-
-//     throw new IllegalArgumentException();
-//   }
-// }
-// class Solution {
-//     public int[] twoSum(int[] nums, int target) {
-//         int n = nums.length;
-//         int[] ans = new int[2];
-//         ans[0] = ans[1] = 0;
-//         HashMap<Integer,Integer> mpp = new HashMap<>(); //elem, index
-//         for(int i =0;i < n; i++){
-//             int num = nums[i];
-//             int moreNeeded = target - num;
-//             if(mpp.containsKey(moreNeeded)){
-//                 ans[0] = mpp.get(moreNeeded);
-//                 ans[1] = i;
-//             }
-//             mpp.put(nums[i],i);
-
-//         }
-//         return ans;
-//     }
-// }
-    
-class Solution{
-    public int[] twoSum(int[] nums, int target)
-    {
-        int n= nums.length;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
         int ans[]= new int[2];
-        HashMap<Integer, Integer> mp= new HashMap<>();
+        ans[0]=-1;
+        ans[1]=-1;
+        int n= nums.length;
+        HashMap<Integer,Integer> mp= new HashMap<>();
         for(int i=0;i<n;i++)
         {
-            int sum1= nums[i];
-            int sum2=target- sum1;
-            if(mp.containsKey(sum2))
+            int curr= nums[i];
+            int want= target-curr;
+            if(mp.containsKey(want))
             {
                 ans[0]=i;
-                ans[1]=mp.get(sum2);
+                ans[1]= mp.get(want);
                 break;
             }
-            else
-            {
-                mp.put(sum1,i);
-            }
+            mp.put(curr,i);
+            
         }
         return ans;
     }
